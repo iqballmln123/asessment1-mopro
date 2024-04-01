c3e1f6b2386823ba42122f144213b7f5079a6713
Chaeza Fauzyyah Inayah <chaeza@student.telkomuniversity.ac.id>
2024-04-01 11:13:59 +0700

    Menambahkan Dark Mode

--- app/src/main/java/org/d3if3056/assesment/MainActivity.kt
+++ app/src/main/java/org/d3if3056/assesment/MainActivity.kt
@@ -1,14 +1,20 @@
 package org.d3if3056.assesment
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import org.d3if3056.assesment.ui.theme.AssesmentTheme
 
@@ -38,6 +44,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 fun GreetingPreview() {
     AssesmentTheme {
