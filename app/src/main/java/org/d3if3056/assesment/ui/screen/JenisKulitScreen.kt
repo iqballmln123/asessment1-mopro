c794958575966b09a03db6b32908df814bfdaec0
Chaeza Fauzyyah Inayah <chaeza@student.telkomuniversity.ac.id>
2024-04-02 14:23:09 +0700

    Membuat screen untuk Jenis Kulit

---
+++ app/src/main/java/org/d3if3056/assesment/ui/screen/JenisKulitScreen.kt
@@ -0,0 +1,208 @@
+package org.d3if3056.assesment.ui.screen
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.Hyphens
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.d3if3056.assesment.R
+import org.d3if3056.assesment.ui.theme.AssesmentTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun JenisKulitScreen() {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Info,
+                            contentDescription = stringResource(id = R.string.info)
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) { padding ->
+        JenisContent(Modifier.padding(padding))
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun JenisContent(modifier: Modifier) {
+    var nama by remember { mutableStateOf("") }
+    var expanded by remember { mutableStateOf(false) }
+    var selectedItemIndex by remember { mutableIntStateOf(0) }
+
+    val jenisKulit = listOf(
+        stringResource(id = R.string.jenis_kulit),
+        stringResource(id = R.string.kulit_normal),
+        stringResource(id = R.string.kulit_kering),
+        stringResource(id = R.string.kulit_berminyak),
+        stringResource(id = R.string.kulit_kombinasi)
+    )
+    val komplikasiKulit = listOf(
+        stringResource(id = R.string.komedo_putih),
+        stringResource(id = R.string.komedo_hitam),
+        stringResource(id = R.string.jerawat),
+        stringResource(id = R.string.keriput)
+    )
+    val checked = remember {
+        mutableStateListOf<Boolean>().apply {
+            repeat(komplikasiKulit.size){
+                add(false)
+            }
+        }
+    }
+
+    if (selectedItemIndex == -1){
+        selectedItemIndex = jenisKulit.indexOf(stringResource(id = R.string.jenis_kulit))
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.intro),
+            style = MaterialTheme.typography.bodyLarge.copy(
+                lineBreak = LineBreak.Paragraph,
+                hyphens = Hyphens.Auto
+            ),
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Justify
+        )
+        OutlinedTextField(
+            value = nama,
+            onValueChange = { nama = it },
+            label = { Text(text = stringResource(id = R.string.nama)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                value = jenisKulit[selectedItemIndex],
+                onValueChange = { },
+                label = { Text(text = stringResource(id = R.string.jenis_kulit)) },
+                readOnly = true,
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .menuAnchor()
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                jenisKulit.forEachIndexed { index, item ->
+                    DropdownMenuItem(
+                        text = {
+                            Text(
+                                text = item,
+                                fontWeight = if (index == selectedItemIndex)
+                                    FontWeight.Bold else null
+                            )
+                        },
+                        onClick = {
+                            selectedItemIndex = index
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+        komplikasiKulit.forEachIndexed{index, stringResId ->
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ){
+                Checkbox(
+                    checked = checked[index],
+                    onCheckedChange = {checked[index] = it},
+                    modifier = Modifier.alignByBaseline()
+                )
+                Text(
+                    text = stringResId,
+//                    modifier = Modifier.padding(start = 8.dp),
+                )
+            }
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+//@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+//@Composable
+//fun ScreenPreview() {
+//    AssesmentTheme {
+//        JenisKulitScreen()
+//    }
+//}
