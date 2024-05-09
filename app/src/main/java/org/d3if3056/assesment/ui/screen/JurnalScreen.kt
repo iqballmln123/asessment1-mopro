7da48cab8e9a79e353b7ea8cb1d2770e9a8d7bc3
Chaeza Fauzyyah Inayah <chaeza@student.telkomuniversity.ac.id>
2024-05-09 14:18:16 +0700

    Membuat tampilan empty state yang berisi gambar dan teks

--- app/src/main/java/org/d3if3056/assesment/ui/screen/JurnalScreen.kt
+++ app/src/main/java/org/d3if3056/assesment/ui/screen/JurnalScreen.kt
@@ -1,6 +1,7 @@
 package org.d3if3056.assesment.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -36,7 +38,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -122,6 +126,12 @@ fun JurnalContent(modifier: Modifier, navController: NavHostController) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.list_kosong),
+                contentDescription = stringResource(id = R.string.list_kosong),
+                modifier = Modifier.size(120.dp),
+                contentScale = ContentScale.Crop
+            )
             Text(text = stringResource(id = R.string.list_kosong))
         }
     } else {
