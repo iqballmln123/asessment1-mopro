7c67164a970f1074f70f2f869ca4386925603bec
Chaeza Fauzyyah Inayah <chaeza@student.telkomuniversity.ac.id>
2024-05-10 11:36:08 +0700

    Menyelesaikan Assessment 2

--- app/src/main/java/org/d3if3056/assesment/ui/screen/JurnalScreen.kt
+++ app/src/main/java/org/d3if3056/assesment/ui/screen/JurnalScreen.kt
@@ -1,7 +1,6 @@
 package org.d3if3056.assesment.ui.screen
 
 import android.content.res.Configuration
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -42,7 +41,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
