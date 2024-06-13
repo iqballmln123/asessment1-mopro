80316bcaef2ab5312f3c627636f496af900e16e4
Chaeza Fauzyyah Inayah <chaeza@student.telkomuniversity.ac.id>
2024-06-13 11:13:22 +0700

    Membuat tampilan nama skincare

--- app/src/main/java/org/d3if3056/assesment/ui/screen/KoleksiScreen.kt
+++ app/src/main/java/org/d3if3056/assesment/ui/screen/KoleksiScreen.kt
@@ -1,8 +1,10 @@
 package org.d3if3056.assesment.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -22,14 +24,18 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -96,7 +102,8 @@ fun ScreenContent(modifier: Modifier){
 @Composable
 fun ListItem(skincare: Skincare){
     Box(
-        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray)
+        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray),
+        contentAlignment = Alignment.BottomCenter
     ){
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
@@ -107,6 +114,24 @@ fun ListItem(skincare: Skincare){
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxWidth().padding(4.dp)
         )
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(4.dp) //antara border dan gambar
+                .background(Color(red = 0f, green = 0f, blue = 0f, alpha = 0.5f))
+                .padding(4.dp)
+        ) {
+            Text(
+                text = skincare.nama,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Text(
+                text = skincare.namaLatin,
+                fontStyle = FontStyle.Italic,
+                fontSize = 14.sp,
+                color = Color.White
+            )
+        }
     }
 }
 
